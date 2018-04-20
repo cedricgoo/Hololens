cdc4f93ec19f7e0a5eb527054534ddc28e023699
Unknown <cedricgo@live.fr>
2018-04-20 17:53:23 +0200

    Version avec lions (2 text/cube) + auto détect des murs

---
+++ ISIB_Plan_Auto/Assets/PlanManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanManager : MonoBehaviour {
+    Texture secondText;
+    // Use this for initialization
+    void Start () {
+		secondText = Resources.Load("Lion") as Texture2D;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(GameObject.Find("SurfacePlanes")){
+            foreach (Transform plane in GameObject.Find("SurfacePlanes").transform)
+            {
+                plane.GetComponent<MeshRenderer>().material.EnableKeyword("_DETAIL_MULX2");
+                plane.GetComponent<MeshRenderer>().material.SetTexture("_DetailAlbedoMap", secondText);
+            }
+        }
+    }
+}
